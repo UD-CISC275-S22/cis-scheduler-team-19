662756e9deec8808aeb27dbcd7f8d6059412cacf
zyjiang@udel.edu <zyjiang@udel.edu>
2022-04-18 08:44:16 -0400

    fixed courseView

--- src/components/courseView.tsx
+++ src/components/courseView.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { Course } from "../interfaces/course";
+import { useState } from "react";
+import { CourseEditor } from "./CourseEditor";
+import { Button, Container, Row, Col, Form } from "react-bootstrap";
 
-export function CourseView({ course }: { course: Course }): JSX.Element {
-    return (
+export function CourseView({ 
+    course,
+    removeCourse,
+    editCourse,
+}: { 
+    course: Course;
+    removeCourse: (id: number) => void
+    editCourse: (id: string, newCourse: Course) => void;
+}): JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+
+    function changeEditing() {
+        setEditing(!editing);
+    }
+        return editing ? (
+            <CourseEditor
+                changeEditing={changeEditing}
+                course={course}
+                editCourse={editCourse}
+                removeCourse={removeCourse}
+            ></CourseEditor>
+        ):(
         <fieldset>
             <table>
                 <thead>
                     <tr>
-                        <th>{course.course_id}</th>
-                        <th>{course.course_title}</th>
-                        <th>{course.course_description}</th>
-                        <th>{course.course_credit}</th>
+                        <th>{course.code}</th>
+                        <th>{course.title}</th>
+                        <th>{course.credit}</th>
+                        <th>{course.description}</th>
                     </tr>
                 </thead>
             </table>
